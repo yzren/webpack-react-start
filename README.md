f94d87fa2c9d13523041b8295757b14b81e18dfe
febobo <584516601@qq.com>
2016-03-16 18:34:46 +0800

    readme.md

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # webpack-react-start
 webpack + react + postcss 环境
+
+### Usage
+
+-step 1
+```
+npm i
+``` 
+
+-step 2  
+```
+npm start  
+```  
+-step 3
+```
+Browser open localhost:3004  
+```
--- src/test.js
+++ src/test.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
+
+// 外链样式
 import s from './test.scss';
 
+// 图片测试
 import url from './logo.png'
 
 s._insertCss();
